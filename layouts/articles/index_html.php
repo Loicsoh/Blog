841bfc1e6f6356f37ac5e5036e88a6b2528ae0f9
Loicsoh <www.sohloic132@gmail.com>
2025-04-28 14:24:02 +0100

    tre

--- layouts/articles/index_html.php
+++ layouts/articles/index_html.php
@@ -1,13 +1,17 @@
-<h1>Listes des articles</h1>
+<h1>Liste des articles</h1>
 
 <hr>
 <div>
-  <?php foreach($articles as $article) :?>
-  <h2> <?= $article['title']; ?> </h2>
-
-  <p> <?= $article['introduction']; ?> </p>
-
-  <a href="article.php?id=<?= $article['id'] ?>">Voir plus</a>
-  <?php endforeach; ?>
+  <?php 
+  // Vérifie si la variable $articles est définie, est un tableau et contient des éléments
+  if (isset($articles) && is_array($articles) && count($articles) > 0) : ?>
+    <?php foreach($articles as $article) : ?>
+      <h2><?= htmlspecialchars($article['title'] ?? ''); ?></h2>
+      <p><?= htmlspecialchars($article['introduction'] ?? ''); ?></p>
+      <a href="article.php?id=<?= htmlspecialchars($article['id'] ?? ''); ?>">Voir plus</a>
+    <?php endforeach; ?>
+  <?php else : ?>
+    <p>Aucun article disponible pour le moment.</p>
+  <?php endif; ?>
 </div>
 <hr>
--- layouts/articles/show_html.php
+++ layouts/articles/show_html.php
@@ -1,9 +1,12 @@
-<h1 class="article-title"> <?= $article['title']; ?> </h1>
-<p> <?= $article['introduction']; ?> </p>
-<p> <?= $article['content']; ?> </p>
+<h1 class="article-title"> <?= htmlspecialchars($article['title'] ?? ''); ?> </h1>
+<p> <?= htmlspecialchars($article['introduction'] ?? ''); ?> </p>
+<p> <?= htmlspecialchars($article['content'] ?? ''); ?> </p>
+<p> <?= htmlspecialchars($article['created_at'] ?? ''); ?> </p>
+<p> <?= htmlspecialchars($article['update_at'] ?? ''); ?> </p>
+<p> <?= htmlspecialchars($article['author'] ?? ''); ?> </p>
 
-<em>Poster le: <?= $article['created_at']; ?> </em>
+<em>Poster le: <?= htmlspecialchars($article['created_at'] ?? ''); ?> </em>
 
-<form>
-<a href="index.php">Retour</a>
+<form action="" class="form">
+  <a href="index.php">Retour</a>
 </form>
